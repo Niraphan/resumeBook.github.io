9039fe0d850f67121821251d2c0e6af80e4808a5
Niraphan <niraphan2560@gmail.com>
2024-10-08 00:36:40 +0700

    Revert "Updating some ui"
    This reverts commit 19dfaa044385f28018ae1ca27e8ce4f95ffe03c4.

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -36,7 +36,7 @@
                     <img src="/icon/location.png" alt="location" class="h-[4vh] w-[4vh] mr-[3vh]" />
                     <p class="text-2xl mb-[1vh]">Location: House No. 64/2, Songklong , Bangpakong , Chachoengsao 24130</p>
                 </div>
-                <iframe class="rounded-md pb-[3vh] w-[70vh] h-[30vh]"
+                <iframe class="rounded-md"
                     src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3879.502694752988!2d100.90160271072197!3d13.504712986807094!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTPCsDMwJzE3LjAiTiAxMDDCsDU0JzE1LjEiRQ!5e0!3m2!1sth!2sth!4v1724693026066!5m2!1sth!2sth"
                     width="600" height="450" style="border: 0" allowfullscreen="" loading="lazy"
                     referrerpolicy="no-referrer-when-downgrade"></iframe>
